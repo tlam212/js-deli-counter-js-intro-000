f71de1c4f82417d735426578a09d1e0296ce11be
Toni Lamont <tlam212@gmail.com>
2018-05-22 19:06:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function currentLine(line) {
   if (katzDeli.length === 0)
   return 'The line is currently empty.'
   else{
-    return 'The line is currently: ' + line;
+    return ('The line is currently: ' + line);
   }
 }
