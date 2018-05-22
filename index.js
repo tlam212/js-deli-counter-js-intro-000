0240611ff66e34b0ff70bcf91422c2a4a725c8c2
Toni Lamont <tlam212@gmail.com>
2018-05-22 19:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLineNew(line) {
   for (let i = 0; i < katzDeli.length; i++) {
     line.push(' ' + [i+1] + '.' + katzDeli [i])
   }
-  if (katzDeli.length === 0) {
+  if (line.length === 0) {
   return 'The line is currently empty.';
   }else{
     return ('The line is currently: ' + line);
