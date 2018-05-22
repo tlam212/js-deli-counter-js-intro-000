5753a39fadf60776c88d605df9b3db99d979c95d
Toni Lamont <tlam212@gmail.com>
2018-05-22 19:11:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function nowServing(katzDeli) {
   }else{ 
     return 'Currently serving ' + katzDeli.shift() + '.';
 }
-}
-function currentLine(line) {
+} let line = [];
+function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
     line.push(' ' + [i+1] + '.' + katzDeli [1])
   }
-  if (katzDeli.length === 0) {
+  if (currentLine.length === 0) {
   return 'The line is currently empty.'
   }else{
     return ('The line is currently: ' + line);
