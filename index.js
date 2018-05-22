93b5c0daeb376f16b40666c7b5c98257b6cfc46f
Toni Lamont <tlam212@gmail.com>
2018-05-22 18:48:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function takeANumber(katzDeliLine, name) {
   console.log('Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.');
   
 } katzDeliLine ('Ada', 'Grace', 'Kent');
+function nowServing(katzDeli) {
+  if (katzDeli.length === 0){
+    return ""
+  }
+}
