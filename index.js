a2fe09a2b1e90d21cfea1feae35bceee713ff6c4
Toni Lamont <tlam212@gmail.com>
2018-05-22 20:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function currentLine(line) {
   if(line.length === 0) {
     return 'The line is currently empty.'; 
   } for (let i = 0; i < line.length; i++) {
-    lineNumber.push(`${i+1}. ${line[i]} `);
-  } return 'The line is currently: ' + lineNumber;
+    lineNumber.push(`${i+1}. ${line[i]}`);
+  } return 'The line is currently: ' + lineNumber ' ';
 }
